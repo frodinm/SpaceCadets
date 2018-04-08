0edd4def6519616a708442c06e1aae5beee24bd5
Anouk <anoukmontreuil@gmail.com>
2018-04-08 00:00:43 -0400

    Added MapNav & Sidebar Components to App...

--- client/src/App.js
+++ client/src/App.js
@@ -15,15 +15,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div><MapNav /></div>
+        <div><Sidebar /></div>
         <Map
           isMarkerShown
           googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
           loadingElement={<div style={{ height: `100%` }} />}
           containerElement={<div style={{ height: `100vh` }} />}
-          mapElement={<div style={{ height: `100%` }} />}
+          mapElement={<div style={{ height: `100%`, zIndex: "-1" }} />}
         />
-        <div><MapNav /></div>
-        <div><Sidebar /></div>
       </div>
     );
   }
